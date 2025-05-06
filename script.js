c51199427cc514d7a047a88430c27e0a357faac7
Maaz-webdeveloper <maazrajpoot991@gmail.com>
2025-05-06 20:17:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["HAPPY", "BIRTHDAY!","to You Hayan"],
+    strings: ["HAPPY", "BIRTHDAY!","to You Maaz"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
